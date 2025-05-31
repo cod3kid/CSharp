2552522d31f9778b40e24f47a3fcd5e75ab19931
cod3kid <djsufail@gmail.com>
2025-05-31 17:50:42 +0400

    Generics in C# again

---
+++ Advanced/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Nullable<T>{
+    
+    private object _value;
+    
+    public Nullable(){}
+    
+    
+    public Nullable(T value){
+        _value = value;
+    }
+    
+    public bool HasValue{
+       get {return _value!=null;}
+    }
+    
+    public T GetValueOrDefault(){
+        if(HasValue){
+            return (T)_value;
+        }
+        
+        return default(T);
+    }
+}
+
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        var number = new Nullable<int>();
+        Console.WriteLine(number.HasValue);
+        Console.WriteLine(number.GetValueOrDefault());
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+Console.WriteLine("Enter a series of numbers separated by hyphens");
+
+string input = Console.ReadLine();
+string[] nums = input.Split('-');
+
+int prev = int.Parse(nums[0]);
+bool flag = true;
+bool isIncreasing = prev < int.Parse(nums[1]);
+
+for (var i = 1; i < nums.Length; i++)
+{
+    var current = int.Parse(nums[i]);
+    if (isIncreasing && prev + 1 != current)
+    {
+        flag = false;
+    }
+
+        if (!isIncreasing && prev - 1 != current)
+    {
+        flag = false;
+    }
+    prev = current;
+}
+
+if (flag==true)
+{
+    Console.WriteLine("Consecutive");
+}
+else
+{
+    Console.WriteLine("Not Consecutive");
+}
